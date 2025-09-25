e7fdab62167b3a914b9bb0a1c172aa2d8c759568
exttreme0001 <fpm.LukashovES@bsu.by>
2025-09-25 20:39:18 +0300

    распределенная база фаза 1

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-
 set -e
-host="postgres"
-port="5432"
-user="postgres"
 
-echo "Waiting PostgreSQL ($host:$port)..."
+# Проверяем, установлена ли переменная окружения DSN
+if [ -z "$DSN" ]; then
+  echo "Error: DSN environment variable is not set." # <-- Исправлено
+  exit 1
+fi
 
-until PGPASSWORD=my_pass pg_isready -h "$host" -p "$port" -U "$user"; do
+echo "Waiting for Neon PostgreSQL database..."
+
+# Используем pg_isready с переменной, полученной из окружения
+until pg_isready -d "$DSN" -q; do
   echo "DB unavailable, waiting..."
   sleep 2
 done
